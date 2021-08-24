db02844e3fede0b2d8c34f46a7e200b021f07a3a
u4ik <insighteuphoric@gmail.com>
2021-08-24 17:08:37 -0400

    update readme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+import Auth from './components/Auth'
 
 
-function App () {
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    return (
-      <div className="App-header">
+  return (
+    <div className="App-header">
+      {
+        loggedIn ?
+          <button style={{ padding: '1em' }} onClick={() => setLoggedIn(false)}>Logout</button>
+          :
+          <Auth loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+          
+      }
 
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
 
 export default App;
---
+++ src/components/Auth.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const Auth = (props) => {
+
+    const [signup, setSignup] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const URL = `https://useracess.herokuapp.com/user/${signup ? 'create' : 'login'}`;
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const reqBody = {
+            email: email,
+            password: password,
+        }
+        try {
+            const res = await fetch(URL, {
+                method: "POST",
+                body: JSON.stringify(reqBody),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+            })
+            const json = await res.json();
+
+            if (json.errors) {
+                throw new Error(json.errors[0].message)
+            } else {
+                console.log(json.Message);
+                props.setLoggedIn(true)
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <>
+            <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={(e) => handleSubmit(e)}>
+
+                <label htmlFor='email'>Email</label>
+                <input required type='email' name='email' id='email' onChange={(e) => setEmail(e.target.value)} />
+
+                <label htmlFor='password'>Password</label>
+                <input  required type='password'  id='password' onChange={(e) => setPassword(e.target.value)} />
+
+
+                <button type='button' style={{ margin: '1em' }} onClick={() => setSignup(!signup)}>{signup ? 'Need to Login?' : 'Need to Signup?'}</button>
+
+                <input type='submit' style={{ margin: '1em' }} >{signup ? 'Signup' : 'Login'}</input>
+
+            </form>
+        </>
+    )
+}
+
+export default Auth;
